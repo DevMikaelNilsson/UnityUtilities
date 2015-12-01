cc0cbf25a3bf8e83c1363f441bc421d8f4848ffa
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2015-12-01 12:21:45 +0100

    Update Translate.cs

--- Transformation/Translate.cs
+++ Transformation/Translate.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-namespace TabUtilities.Transformation
+namespace UnityUtilities.Transformation
 {
 	[AddComponentMenu("Transformation/Translate")]
 	public class Translate : MonoBehaviour 
