d7236f22a22a9337bad032db5371a24b0f4ab721
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2015-12-04 08:48:20 +0100

    Update FollowPathfinder.cs

--- Pathfinding/FollowPathfinder.cs
+++ Pathfinding/FollowPathfinder.cs
@@ -51,8 +51,8 @@ namespace mnUtilities.Pathfinding
 		void OnEnable () 
 		{
 			if(this.GetComponent<Pathfinder>() != null)
-				Debug.LogWarning(this + " - This object has a Pathfinder component attached to it. This can cause unexpected behavior and errors and its not recommended to have both components attached to the same object.")
-			
+				Debug.LogWarning(this + " - This object has a Pathfinder component attached to it. This can cause unexpected behavior and errors and its not recommended to have both components attached to the same object.");
+
 			if(m_transformComponent == null)
 				m_transformComponent = this.GetComponent<Transform>();
 				
