3b875d8b72958c306424449c9c2af1d21e52eade
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2015-12-23 08:49:09 +0100

    Create BaseEditor.cs

---
+++ EditorUtillities/BaseEditor.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EditorUtillities
+{
+	public class BaseEditor : Editor
+	{
+		public void CreatePropertyField(SerializedProperty currentProperty, string text, string tooltipText)
+		{
+			GUIContent content = new GUIContent(text, tooltipText);
+			EditorGUILayout.PropertyField(currentProperty, content);
+		}
+
+		public bool CreateButton(string text, string tooltipText)
+		{
+			GUIContent content = new GUIContent(text, tooltipText);
+			return GUILayout.Button(content);
+		}
+
+		public List<string> LoadMethodsFromGameObject(Behaviour []ListOfMethods)
+		{
+			List<string> listOfMethods = new List<string>();
+			int objectCount = ListOfMethods.Length;
+			char[] Seperators = new char[2];
+			Seperators[0] = '(';
+			Seperators[1] = ')';
+			for(int i = 0; i < objectCount; ++i)
+			{
+				string[] listOfTempNameStrings = ListOfMethods[i].ToString().Split(Seperators);
+				listOfMethods.Add(listOfTempNameStrings[1]);
+			}
+
+			return listOfMethods;
+		}
+
+		public int CreatePopupList(string []currentArray, int currentIndex, string text, string tooltipText)
+		{
+			if(currentArray != null)
+			{
+				GUIContent content = new GUIContent(text, tooltipText);
+				GUIContent []displayedOptions = new GUIContent[currentArray.Length];
+				for(int i = 0; i < displayedOptions.Length; ++i)
+				{
+					displayedOptions[i] = new GUIContent(currentArray[i]);
+				}
+
+				return EditorGUILayout.Popup(content, currentIndex, displayedOptions);
+			}
+
+			return 0;
+		}
+
+		public int GetCurrentBehaviourIndex(Behaviour []methodList, Behaviour currentBehaviour)
+		{
+			if(currentBehaviour != null)
+			{
+				int objectCount = methodList.Length;
+				for(int i = 0; i < objectCount; ++i)
+				{
+					if(string.Equals(methodList[i].ToString(), currentBehaviour.ToString()) == true)
+						return i;
+				}
+			}
+
+			return 0;
+		}
+	}
+}
