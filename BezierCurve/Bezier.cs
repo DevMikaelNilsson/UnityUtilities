2a4a90d488f2282d8cc2fb0572ced76d081949e9
DevMikaelNilsson <DevMikaelNilsson@outlook.com>
2016-02-11 07:48:15 +0100

    Update Bezier.cs

--- BezierCurve/Bezier.cs
+++ BezierCurve/Bezier.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/*
+	This code is based on the tutorial and source code from Catlike Coding.
+	Link: http://catlikecoding.com/unity/tutorials/curves-and-splines/
+*/
 namespace mnUtilities.BezierCurve
 {
 	public static class Bezier 
